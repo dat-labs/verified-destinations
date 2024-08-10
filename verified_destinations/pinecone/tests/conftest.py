28b20a8676c0a43db7c4d2d22232834c83ee02f1
suryaanshrai <suryaanshrai@gmail.com>
2024-08-10 22:41:12 +0530

    Added tests

--- verified_destinations/pinecone/tests/conftest.py
+++ verified_destinations/pinecone/tests/conftest.py
@@ -1,33 +1,132 @@
 import os
 import pytest
-from dat_core.pydantic_models.connector_specification import ConnectorSpecification
-from dat_core.pydantic_models import DatCatalog, DatDocumentStream, ReadSyncMode, WriteSyncMode
+from time import sleep
+from pinecone import Pinecone
 
+NAMESPACE_TEST = 'pytest'
 
-@pytest.fixture()
-def valid_connection_specification():
+@pytest.fixture(scope='function')
+def valid_connection_specification(request):
+    pinecone_api_key = os.getenv('PINECONE_API_KEY')
+    pinecone_index = os.getenv('PINECONE_INDEX')
+    pinecone_environment = os.getenv('PINECONE_ENVIRONMENT')
+    embedding_dimensions = os.getenv('EMBEDDING_DIMENSIONS')
+
     yield {
-        'pinecone_index': os.getenv('PINECONE_INDEX'),
-        'pinecone_api_key': os.getenv('PINECONE_API_KEY'),
-        'pinecone_environment': os.getenv('PINECONE_ENVIRONMENT'),
-        'embedding_dimensions': os.getenv('PINECONE_EMBEDDING_DIMENSIONS'),
+        'pinecone_index': pinecone_index,
+        'pinecone_api_key': pinecone_api_key,
+        'pinecone_environment': pinecone_environment,
+        'embedding_dimensions': embedding_dimensions,
     }
 
-@pytest.fixture()
-def valid_document_streams():
-    yield {
-        'document_streams': [
-            {
-                'name': 'PDF',
-                'namespace': 'pytest_pdf',
+@pytest.fixture(scope='function')
+def valid_catalog_object(request):
+    write_sync_modes = ['APPEND', 'REPLACE', 'UPSERT']
+    document_streams = { 'document_streams': [] }
+    
+    for name in ['PDF', 'CSV']:
+        for write_sync_mode in write_sync_modes:
+            document_streams['document_streams'].append({
+                'name': name,
+                'namespace': NAMESPACE_TEST,
                 'read_sync_mode': 'INCREMENTAL',
-                'write_sync_mode': 'UPSERT',
-            },
+                'write_sync_mode': write_sync_mode,
+            })
+
+    yield document_streams
+
+old_record_metadata = [
+    {   
+        'meta': 'Objective', 
+        'dat_source': 'S3',
+        'dat_stream': 'PDF',
+        'dat_document_entity': 'DBT/DBT Overview.pdf',
+    },
+    {
+        'meta': 'Arbitrary', 
+        'dat_source': 'S3',
+        'dat_stream': 'PDF',
+        'dat_document_entity': 'Google/Tutorials search_algorithms.pdf',
+    },
+]
+
+new_record_metadata = [
+    {
+        'meta': 'Objective', 
+        'dat_source': 'S3',
+        'dat_stream': 'PDF',
+        'dat_document_entity': 'DBT/DBT Overview.pdf'
+    },
+    {
+        'meta': 'Arbitrary', 
+        'dat_source': 'S3',
+        'dat_stream': 'PDF',
+        'dat_document_entity': 'DAT/docs Introduction.pdf'
+    },
+    {
+        'meta': 'Objective', 
+        'dat_source': 'S3',
+        'dat_stream': 'CSV',
+        'dat_document_entity': 'Apple/DBT/DBT Employee.csv'
+    },
+]
+
+def record_list(write_sync_mode='APPEND', category='NEW'):
+    """
+    Returns a list of valid records.
+
+    Args:
+        write_sync_mode (str, optional): Possible values: ['APPEND', 'REPLACE', 'UPSERT']. Defaults to 'APPEND'.
+        category (str, optional): Possible values: ['NEW', 'OLD']. Defaults to 'NEW'.
+    """
+    embedding_dimensions = int(os.getenv('EMBEDDING_DIMENSIONS'))
+    if not embedding_dimensions:
+        raise ValueError('EMBEDDING_DIMENSIONS is not set')
+    
+    extra_metadata = {}
+
+    if category == 'NEW':
+        record_metadata = new_record_metadata
+        extra_metadata['document_chunk'] = 'New Data'
+    elif category == 'OLD':
+        record_metadata = old_record_metadata
+        extra_metadata['document_chunk'] = 'Old Data'
+
+    vectors = [1.0] * embedding_dimensions
+
+    records = list()
+    for metadata in record_metadata:
+        metadata.update(extra_metadata)
+        records.append(
             {
-                'name': 'CSV',
-                'namespace': 'pytest_csv',
-                'read_sync_mode': 'INCREMENTAL',
-                'write_sync_mode': 'APPEND',
+                'type': 'RECORD',
+                'record': {
+                    'data': {
+                        'document_chunk': metadata['document_chunk'],
+                        'vectors': vectors,
+                        'metadata': metadata
+                    },
+                    'emitted_at': 1,
+                    'namespace': NAMESPACE_TEST,
+                    'stream': {
+                        'name': metadata['dat_stream'],
+                        'namespace': NAMESPACE_TEST,
+                        'read_sync_mode': 'INCREMENTAL',
+                        'write_sync_mode': write_sync_mode
+                    }
+                }
             }
-        ]
-    }
+        )
+    return records
+
+def pytest_sessionfinish(session, exitstatus):
+    pinecone_api_key = os.getenv('PINECONE_API_KEY')
+    pinecone_index = os.getenv('PINECONE_INDEX')
+    if pinecone_api_key and pinecone_index:
+        client = Pinecone(api_key=pinecone_api_key)
+        index = client.Index(pinecone_index)
+        index_namespaces = index.describe_index_stats()['namespaces'].keys()
+        if NAMESPACE_TEST in index_namespaces:
+            index.delete(deleteAll=True, namespace=NAMESPACE_TEST)
+    else:
+        print('Pinecone API key or index not set. Skipping teardown.')
--- verified_destinations/pinecone/tests/test_pinecone.py
+++ verified_destinations/pinecone/tests/test_pinecone.py
@@ -6,14 +6,32 @@ from dat_core.pydantic_models import (
     Data, DatStateMessage,
     StreamState, StreamStatus,
     DatDocumentStream, Type,
-    DatCatalog, Status
+    DatCatalog, Status,
+    StreamMetadata,
 )
 from verified_destinations.pinecone.destination import Pinecone
 from verified_destinations.pinecone.specs import PineconeSpecification
+from verified_destinations.pinecone.tests.conftest import record_list, NAMESPACE_TEST
+from pinecone import Pinecone as PC
 
+from time import sleep
 
 class TestPinecone:
-
+    def test_env(self, valid_connection_specification):
+        """
+        Checks if all the values in the `valid_connection_specification` dictionary are not empty.
+        If any value is empty, raises a `ValueError` with a message indicating which key is missing.
+        
+        :param valid_connection_specification: A dictionary containing the connection specification.
+        :type valid_connection_specification: dict
+        
+        :raises ValueError: If any value in the `valid_connection_specification` dictionary is empty.
+        """
+        for key,value in valid_connection_specification.items():
+            if not value:
+                raise ValueError(f"Missing {key}. Please set it as an environment variable and try again.")
+            
+            
     def test_spec(self, ):
         """
         GIVEN None
@@ -39,109 +57,56 @@ class TestPinecone:
                 module_name='pinecone',
                 connection_specification=valid_connection_specification
             ))
-        print(check)
         assert check.status == Status.SUCCEEDED
 
-    def test_write(self, valid_connection_specification, valid_document_streams):
+    def test_write(self, valid_connection_specification, valid_catalog_object):
         """
         GIVEN a valid connectionSpecification JSON config
         WHEN write() is called on a valid Destination class
         THEN no error is raised
         """
+        clear_namespaces(valid_connection_specification)
+
         config = PineconeSpecification(
             name='Pinecone',
             module_name='pinecone',
             connection_specification=valid_connection_specification
         )
-        configured_catalog = DatCatalog(**valid_document_streams)
-        first_record = DatMessage(
-                type=Type.RECORD,
-                record=DatDocumentMessage(
-                    data=Data(
-                        document_chunk='foo',
-                        vectors=[0.1] * config.connection_specification.embedding_dimensions,
-                        metadata={"meta": "Objective", "dat_source": "S3",
-                                  "dat_stream": "PDF", "dat_document_entity": "DBT/DBT Overview.pdf"},
-                    ),
-                    emitted_at=1,
-                    namespace=configured_catalog.document_streams[0].namespace,
-                    stream=DatDocumentStream(
-                        name=configured_catalog.document_streams[0].name,
-                        namespace=configured_catalog.document_streams[0].namespace,
-                        read_sync_mode="INCREMENTAL",
-                        write_sync_mode="REPLACE",
-                    ),
-                ),
-            )
-        second_record = DatMessage(
-                type=Type.RECORD,
-                record=DatDocumentMessage(
-                    data=Data(
-                        document_chunk='bar',
-                        vectors=[1.0] * config.connection_specification.embedding_dimensions,
-                        metadata={"meta": "Arbitrary", "dat_source": "S3",
-                                  "dat_stream": "CSV", "dat_document_entity": "Apple/DBT/DBT Overview.pdf"},
-                    ),
-                    emitted_at=2,
-                    namespace=configured_catalog.document_streams[1].namespace,
-                    stream=DatDocumentStream(
-                        name=configured_catalog.document_streams[1].name,
-                        namespace=configured_catalog.document_streams[1].namespace,
-                        read_sync_mode="INCREMENTAL",
-                        write_sync_mode="REPLACE",
-                    )
-                ),
-            )
-        mocked_input: List[DatMessage] = [
-            first_record,
-            second_record,
-        ]
+        
+        records = record_list(category='NEW', write_sync_mode='APPEND')
+        mocked_input: List[DatMessage] = [DatMessage(**record) for record in records]
+        
+        configured_catalog = DatCatalog(**valid_catalog_object)
         docs = Pinecone().write(
             config=config,
             configured_catalog=configured_catalog,
             input_messages=mocked_input
         )
-        for doc in docs:
-            print(f"doc: {doc}")
-            assert isinstance(doc, DatMessage)
+        sleep(3)
+        
+        for doc in docs: assert isinstance(doc, DatMessage)
+        
+        # Checking if the records stored in Pinecone is same 
+        # as the records passed in the write() call
+        query_records = get_query(
+                valid_connection_specification=valid_connection_specification,
+                namespace=NAMESPACE_TEST, 
+                top_k=len(records)+1, 
+                filter={},
+            )
+        assert len(query_records['matches']) == len(records)
 
-    def test_write_multiple_streams(self, config, conf_catalog):
+    def test_write_multiple_streams(self, valid_connection_specification, valid_catalog_object):
         """
         GIVEN a valid connectionSpecification JSON config
         WHEN write() is called on a valid Destination class
         THEN no error is raised
         """
         comp_state_msgs = []
-        configured_catalog = DatCatalog.model_validate_json(
-            conf_catalog.json())
-        first_record = DatMessage(
-            type=Type.RECORD,
-            record=DatDocumentMessage(
-                data=Data(
-                    document_chunk='foo',
-                    vectors=[1.0] * 1536,
-                    metadata={"meta": "Objective", "dat_source": "S3",
-                                "dat_stream": "PDF", "dat_document_entity": "DBT/DBT Overview.pdf"},
-                ),
-                emitted_at=1,
-                namespace=configured_catalog.document_streams[0].namespace,
-                stream=configured_catalog.document_streams[0]
-            ),
-        )
-        second_record = DatMessage(
-            type=Type.RECORD,
-            record=DatDocumentMessage(
-                data=Data(
-                    document_chunk='bar',
-                    vectors=[1.1] * 1536,
-                    metadata={"meta": "Arbitrary", "dat_source": "S3",
-                                "dat_stream": "CSV", "dat_document_entity": "Apple/DBT/DBT Overview.pdf"},
-                ),
-                emitted_at=2,
-                namespace=configured_catalog.document_streams[1].namespace,
-                stream=configured_catalog.document_streams[1]
-            ),
-        )
+        configured_catalog = DatCatalog(**valid_catalog_object)
+        records = record_list()
+        first_record = DatMessage(**records[0])
+        second_record = DatMessage(**records[1])
         mocked_input: List[DatMessage] = [
             DatMessage(
                 type=Type.STATE,
@@ -188,6 +153,13 @@ class TestPinecone:
                 ),
             ),
         ]
+
+        config = PineconeSpecification(
+            name='Pinecone',
+            module_name='pinecone',
+            connection_specification=valid_connection_specification
+        )
+
         docs = Pinecone().write(
             config=config,
             configured_catalog=configured_catalog,
@@ -202,3 +174,184 @@ class TestPinecone:
         assert len(comp_state_msgs) == 2
         assert comp_state_msgs[0].state.stream_state.stream_status == StreamStatus.COMPLETED
         assert comp_state_msgs[1].state.stream_state.stream_status == StreamStatus.COMPLETED
+
+    def test_write_replace(self, valid_connection_specification, valid_catalog_object):
+        """
+        GIVEN a valid connectionSpecification JSON config
+        WHEN write() is called on a valid Destination class with replace mode
+        THEN no error is raised and the document is replaced
+        """
+        clear_namespaces(valid_connection_specification)
+        config = PineconeSpecification(
+            name='Pinecone',
+            module_name='pinecone',
+            connection_specification=valid_connection_specification
+        )
+
+        configured_catalog = DatCatalog(**valid_catalog_object)
+        old_records = record_list(category='OLD', write_sync_mode='APPEND')
+        mocked_input: List[DatMessage] = [DatMessage(**record) for record in old_records]
+        docs = Pinecone().write(
+            config=config,
+            configured_catalog=configured_catalog,
+            input_messages=mocked_input
+        )
+        sleep(3)
+
+        new_records = record_list(category='NEW', write_sync_mode='REPLACE')
+        mocked_input: List[DatMessage] = [DatMessage(**record) for record in new_records]
+        docs = Pinecone().write(
+            config=config,
+            configured_catalog=configured_catalog,
+            input_messages=mocked_input
+        )
+        sleep(3)
+
+        for doc in docs:
+            assert isinstance(doc, DatMessage)
+        
+        query_result = get_query(
+            valid_connection_specification=valid_connection_specification,
+            namespace=NAMESPACE_TEST, 
+            top_k=10,
+            filter={},
+        )
+        
+        assert len(query_result['matches']) == len(new_records)
+        
+        
+    def test_write_append(self, valid_connection_specification, valid_catalog_object):
+        """
+        GIVEN a valid connectionSpecification JSON config
+        WHEN write() is called on a valid Destination class with append mode
+        THEN no error is raised and the document is appended
+        """
+        clear_namespaces(valid_connection_specification)
+        config = PineconeSpecification(
+            name='Pinecone',
+            module_name='pinecone',
+            connection_specification=valid_connection_specification
+        )
+
+        configured_catalog = DatCatalog(**valid_catalog_object)
+        old_records = record_list(category='OLD', write_sync_mode='APPEND')
+        mocked_input: List[DatMessage] = [DatMessage(**record) for record in old_records]
+        docs = Pinecone().write(
+            config=config,
+            configured_catalog=configured_catalog,
+            input_messages=mocked_input
+        )
+        sleep(3)
+
+        new_records = record_list(category='NEW', write_sync_mode='APPEND')
+        mocked_input: List[DatMessage] = [DatMessage(**record) for record in new_records]
+        docs = Pinecone().write(
+            config=config,
+            configured_catalog=configured_catalog,
+            input_messages=mocked_input
+        )
+        sleep(3)
+
+        for doc in docs:
+            assert isinstance(doc, DatMessage)
+        
+        query_result = get_query(
+            valid_connection_specification=valid_connection_specification,
+            namespace=NAMESPACE_TEST, 
+            top_k=10,
+            filter={},
+        )
+        
+        assert len(query_result['matches']) == len(new_records) + len(old_records)
+    
+    def test_write_upsert(self, valid_connection_specification, valid_catalog_object):
+        """
+        GIVEN a valid connectionSpecification JSON config
+        WHEN write() is called on a valid Destination class with upsert mode
+        THEN no error is raised and the document is upserted
+        """
+        clear_namespaces(valid_connection_specification)
+        config = PineconeSpecification(
+            name='Pinecone',
+            module_name='pinecone',
+            connection_specification=valid_connection_specification
+        )
+
+        configured_catalog = DatCatalog(**valid_catalog_object)
+        old_records = record_list(category='OLD', write_sync_mode='APPEND')
+        mocked_input: List[DatMessage] = [DatMessage(**record) for record in old_records]
+        docs = Pinecone().write(
+            config=config,
+            configured_catalog=configured_catalog,
+            input_messages=mocked_input
+        )
+        sleep(3)
+
+        new_records = record_list(category='NEW', write_sync_mode='UPSERT')
+        mocked_input: List[DatMessage] = [DatMessage(**record) for record in new_records]
+        docs = Pinecone().write(
+            config=config,
+            configured_catalog=configured_catalog,
+            input_messages=mocked_input
+        )
+        sleep(3)
+
+        for doc in docs:
+            assert isinstance(doc, DatMessage)
+        
+        query_result = get_query(
+            valid_connection_specification=valid_connection_specification,
+            namespace=NAMESPACE_TEST, 
+            top_k=10,
+            filter={},
+        )
+        
+        uniques = set()
+        for record in old_records:
+            uniques.add(record['record']['data']['metadata']['dat_document_entity'])
+        for record in new_records:
+            uniques.add(record['record']['data']['metadata']['dat_document_entity'])
+
+        assert len(query_result['matches']) == len(uniques)
+    
+
+
+def pinecone_client(valid_connection_specification, connection_specification=None):
+    if connection_specification:
+        pinecone_client = PC(
+            api_key=connection_specification['pinecone_api_key'],
+            environment=connection_specification['pinecone_environment']
+            )
+    else:
+        pinecone_client = PC(
+            api_key=valid_connection_specification['pinecone_api_key'],
+            environment=valid_connection_specification['pinecone_environment']
+            )
+    return pinecone_client  
+
+def clear_namespaces(valid_connection_specification):
+    client = pinecone_client(valid_connection_specification)
+    index = client.Index(valid_connection_specification['pinecone_index'])
+    index_namespaces = index.describe_index_stats()['namespaces'].keys()
+    if NAMESPACE_TEST in index_namespaces:
+        index.delete(deleteAll=True, namespace=NAMESPACE_TEST)
+    sleep(3)
+
+def get_metadata_filter(metadata):
+    metadata_filter = {}
+    for metadata_key, metadata_value in metadata.items():
+        metadata_filter[metadata_key] = {"$eq": metadata_value}
+    return metadata_filter
+
+def get_query(valid_connection_specification, namespace, top_k, filter):
+    client = pinecone_client(valid_connection_specification)
+    index = client.Index(valid_connection_specification['pinecone_index'])
+    response = index.query(
+        namespace=namespace,
+        filter=get_metadata_filter(filter),
+        vector=[1]*int(valid_connection_specification['embedding_dimensions']),
+        top_k=top_k,
+        include_values=True,
+        include_metadata=True
+    )
+    return response
